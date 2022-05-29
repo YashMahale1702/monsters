8906afbdd43e2be8a9b8e71c1130d4691f58076c
Yash Mahale <yashmahale1702@gmail.com>
2022-05-29 13:35:17 +0530

    Loading state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import './App.scss';
-import { Component } from 'react';
-import { SearchBox } from './components/searchbox/searchbox.compnent';
-import { CardList } from './components/card-list/card-list.component';
+import "./App.scss";
+import { Component } from "react";
+import { SearchBox } from "./components/searchbox/searchbox.compnent";
+import { CardList } from "./components/card-list/card-list.component";
+import { Loading } from "./components/loading/loading.component";
+import { motion } from "framer-motion";
+
+const item = {
+	visible: { opacity: 1, y: 0, transition: { duration: 1 } },
+	hidden: { opacity: 0, y: 100, transition: { duration: 1 } },
+};
 
 class App extends Component {
 	constructor() {
@@ -9,30 +16,49 @@ class App extends Component {
 
 		this.state = {
 			monsters: [],
-			searchField: ''
+			searchField: "",
+			isInit: false,
 		};
 	}
 
 	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/users').then((response) => response.json()).then((users) => {
-			console.log(users);
-			this.setState({
-				monsters: users
+		fetch("https://jsonplaceholder.typicode.com/users")
+			.then((response) => response.json())
+			.then((users) => {
+				console.log(users);
+				this.setState({
+					monsters: users,
+				});
+			})
+			.then(() => {
+				this.setState({
+					isInit: true,
+				});
 			});
-		});
 	}
 
 	render() {
 		const { monsters, searchField } = this.state;
-		const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+		const filteredMonsters = monsters.filter((monster) =>
+			monster.name.toLowerCase().includes(searchField.toLowerCase())
+		);
 
-
-		return (
-			<div className="App">
-				<div className='App_heading'>Monsters</div>
-				<SearchBox placeholder='Search Monsters ...' handleChange={e => this.setState({ searchField: e.target.value })} />
+		return this.state.isInit ? (
+			<motion.div
+				className="App"
+				initial="hidden"
+				animate="visible"
+				variants={item}
+			>
+				<div className="App_heading">Monsters</div>
+				<SearchBox
+					placeholder="Search Monsters ..."
+					handleChange={(e) => this.setState({ searchField: e.target.value })}
+				/>
 				<CardList monsters={filteredMonsters} />
-			</div>
+			</motion.div>
+		) : (
+			<Loading />
 		);
 	}
 }
---
+++ src/components/loading/loading.component.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./loading.styles.scss";
+
+export const Loading = () => {
+	return <div className="center-text">Loading data...</div>;
+};
